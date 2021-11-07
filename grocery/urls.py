a14daa9b75b6f4d5b71b1db76450c4a4da0d3f9f
sathinenisnigdhareddy <sathineniandfamily5@gmail.com>
2021-11-07 14:08:47 +0530

    commiting

--- grocery/urls.py
+++ grocery/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
 	path('register/', views.registerPage, name="register"),
 	path('grocery/<str:pk>/', views.grocery, name="grocery"),
 	path('home/', views.home, name="home"),
+	path('register/', views.registerPage, name="register"),
+	path('result/<int:pk>/', views.result, name="result"),
+ 
+    path("login/", views.login_request, name="login"),
+	
 ]
--- grocery/views.py
+++ grocery/views.py
@@ -4,11 +4,36 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect 
 from django.http import HttpResponse
 from .models import *
+from .forms import  CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+import joblib
+from models_and_data import model
 
 
 def registerPage(request):
+	form = CreateUserForm()
 
-	return render(request,'register.html')
+	if request.method == 'POST':
+		print("hello")
+		form = CreateUserForm(request.POST)
+		
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password1')
+			role = form.cleaned_data.get('first_name')
+			print("username=",username,"password=",password,"role=",role)
+			
+			
+
+			messages.success(request, 'Account was created for ' + username)
+			return redirect('login')
+	context = {'form':form}
+
+
+
+	return render(request, 'register.html', context)
 def grocery(request,pk):
 	objs=grocery_store.objects.all()
 	list=[]
@@ -26,3 +51,44 @@ def grocery(request,pk):
 	return render (request,"grocery.html",num)
 def home(request):
 	return render (request,"home.html")
+def login_request(request):
+	if request.method == 'POST':
+		
+		username = request.POST.get('username')
+		password =request.POST.get('password1')
+		role = request.POST.get('first_name')
+	
+
+		
+		
+		
+
+		user = authenticate(request, username=username, password=password)
+
+
+
+		
+
+		if user is not None:
+			print("hey yo....",user.id)
+			context={'userid':user.id}
+
+			return render(request, 'home.html', context)
+
+		else:
+			messages.info(request, 'Username OR password is incorrect')
+
+	context = {}
+	return render(request, 'login.html', context)
+
+def result(request,pk):
+	x=joblib.load("finalized_model.sav")
+	lis=pk
+	print(lis)
+	print(model.fun(lis)['Title'].head())
+	
+
+	
+
+	
+	return render(request,"result.html")
